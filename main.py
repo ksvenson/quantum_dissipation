87e680c3b94a5ed22f4846fbf1b6ee17457f2a99
ksvenson <kai.svenson628@gmail.com>
2023-12-11 17:24:45 -0500

    sparce eigenvalues - not debugged

--- main.py
+++ main.py
@@ -15,57 +15,48 @@ plot_U = 10*gamma
 
 
 def ham(delta, F, U, N):
-    """
-    Hamiltonian for a Kerr nonlinear resonator
-    :param delta: Resonant-drive detuning
-    :param F: Forcing amplitude, divided by sqrt(N)
-    :param U: Kerr nonlinearity, divided by N
-    :return: Hamiltonian
-    """
     return -delta*ad*a + (U/N/2)*ad*ad*a*a + F*np.sqrt(N)*(ad + a)
 
 
-def scaled_photon_number(delta, F, U, N):
-    tlist = [0]
-    H = ham(delta, F, U, N)
-    rho_ss = steadystate(H, c_ops)
-    e_ops = [ad*a]
-    return mesolve(H, rho_ss, tlist, c_ops, e_ops).expect[0][0]/N
+def scaled_photon_number(state):
+    return np.trace(ad*a*state)/N
 
 
-def evals(delta, F, U, N):
-    H = ham(delta, F, U, N)
-    liouv = liouvillian(H, c_ops)
-    return np.linalg.eigvals(liouv)
-
-
-def gap(delta, F, U, N):
-    return np.sort(-np.real(evals(delta, F, U, N)))[1]
+def liouv_gap(op):
+    return np.abs(np.real(sp.sparse.linalg.eigs(op, k=1, which='SR', return_eigenvectors=False, tol=0.5)[0]))
 
 
 def first_fidelity(delta, F, U, N):
-    tlist = [0]
     H = ham(delta, F, U, N)
     rho_ss = steadystate(H, c_ops)
 
 
-flist = np.linspace(0, 5, 25)
+if __name__ == '__main__':
+    flist = np.linspace(0, 5, 100)
 
-# TODO
-# What does N represent in this case?
-# Thermodynamic parameter
-for N in (1, 3, 10, 20):
-    plt.figure(num='scaled_photon_number')
-    plt.plot(flist, [scaled_photon_number(plot_delta, gamma*f, plot_U, N) for f in flist], label=rf'$N={N}$')
-    plt.ylabel(r'$\langle a^\dag a \rangle/N$')
-    plt.xlabel(r'$\tilde{F}/\gamma$')
-    plt.legend()
+    for N in (1, 3, 10, 20):
+        spn = []
+        gap = []
+        for f in flist:
+            H = ham(plot_delta, gamma*f, plot_U, N)
+            liouv = liouvillian(H, c_ops)
+            rho_ss = steadystate(H, c_ops)
+            spn.append(scaled_photon_number(rho_ss))
+            gap.append(liouv_gap(liouv.data))
+        spn = np.array(spn)
+        gap = np.array(gap)
 
-    plt.figure(num='gap')
-    plt.plot(flist, [gap(plot_delta, gamma*f, plot_U, N)/gamma for f in flist], label=rf'$N={N}$')
-    plt.ylabel(r'$-\text{Re}(\lambda_1/\gamma)$')
-    plt.xlabel(r'$\tilde{F}/\gamma$')
-    plt.yscale('log')
-    plt.legend()
+        # plt.figure(num='scaled_photon_number')
+        # plt.plot(flist, spn, label=rf'$N={N}$')
+        # plt.ylabel(r'$\langle a^\dag a \rangle/N$')
+        # plt.xlabel(r'$\tilde{F}/\gamma$')
+        # plt.legend()
 
-plt.show()
+        plt.figure(num='gap')
+        plt.plot(flist, gap/gamma, label=rf'$N={N}$')
+        plt.ylabel(r'$-\text{Re}(\lambda_1/\gamma)$')
+        plt.xlabel(r'$\tilde{F}/\gamma$')
+        plt.yscale('log')
+        plt.legend()
+
+    plt.show()
