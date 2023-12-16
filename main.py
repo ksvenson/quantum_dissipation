41cac4cffc5cbd237118c9ba52bc3cc87705ce1f
ksvenson <kai.svenson628@gmail.com>
2023-12-15 23:10:36 -0500

    calculating evals without using sparse

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 
-dim = 30
+dim = 60
 w0 = 1  # resonant frequency
 gamma = 0.1  # dampening rate
 a = destroy(dim)
@@ -12,6 +12,8 @@ c_ops = [np.sqrt(gamma/2) * a]
 
 plot_delta = 10*gamma
 plot_U = 10*gamma
+flist = np.linspace(0, 5, 100)
+Nlist = np.array([1, 3, 10, 20])
 
 
 def ham(delta, F, U, N):
@@ -24,11 +26,17 @@ def scaled_photon_number(state):
 
 def liouv_gap(op):
     print('calculating eigenvalues')
-    # eigs = np.sort(np.abs(np.real(np.linalg.eigvals(op))))[1]
-    # return np.abs(np.real(eigs))
-    evals, evecs = sp.sparse.linalg.eigs(op.data, k=1, which='SR', tol=0.5)
-    # eigs = sp.sparse.linalg.eigs(op.data, k=1, sigma=0, return_eigenvectors=False, tol=0.5)
-    return evals[0], evecs[:, 0]
+    eigs = np.sort(np.abs(np.real(np.linalg.eigvals(op))))[1]
+    return eigs
+    # evals = sp.sparse.linalg.eigs(sp.linalg.fractional_matrix_power(op, 0.1), k=1, which='LR', tol=10**(-4), return_eigenvectors=False)
+    # # eigs = sp.sparse.linalg.eigs(op.data, k=1, sigma=0, return_eigenvectors=False, tol=0.5)
+    # print(evals)
+    # idx = np.argmin(np.real(evals))
+    # return evals[idx]
+
+
+def get_evals(op):
+    return np.linalg.eigvals(op)
 
 
 def fidelity(s1, s2):
@@ -36,28 +44,51 @@ def fidelity(s1, s2):
     return np.trace(sp.linalg.sqrtm(sqrts1 * s2 * sqrts1))
 
 
-if __name__ == '__main__':
-    flist = np.linspace(0, 5, 25)
+def wigner():
+    # rho+
+    # rho -
+    # rho steady state
+    pass
 
-    for N in (1, 3, 10, 20):
+
+def save_evals():
+
+    return  # don't want to accidentally call this function
+
+    np.save('flist', flist)
+    np.save('Nlist', Nlist)
+
+    result = np.zeros((len(Nlist), len(flist), dim**2), dtype=complex)
+    for Nidx, N in enumerate(Nlist):
+        for fidx, f in enumerate(flist):
+            H = ham(plot_delta, gamma * f, plot_U, N)
+            liouv = liouvillian(H, c_ops)
+            result[Nidx, fidx] = get_evals(liouv)
+            np.save(f'evals_plotdelta_{plot_delta}_plotU_{plot_U}', result)
+            print(f'Done (N, f) = {(N, f)}')
+
+
+if __name__ == '__main__':
+    all_evals = np.load(f'evals_plotdelta_{plot_delta}_plotU_{plot_U}.npy')
+    for Nidx, N in enumerate(Nlist):
         spn = []
         gap = []
-        for f in flist:
+        for fidx, f in enumerate(flist):
             H = ham(plot_delta, gamma*f, plot_U, N)
             liouv = liouvillian(H, c_ops)
             rho_ss = steadystate(H, c_ops)
-            gap_eval, gap_evec = liouv_gap(liouv)
+            evals = all_evals[Nidx, fidx]
 
             spn.append(scaled_photon_number(rho_ss))
-            gap.append(np.abs(np.real(gap_eval)))
+            gap.append(np.sort(-np.real(evals))[1])
         spn = np.array(spn)
         gap = np.array(gap)
 
-        # plt.figure(num='scaled_photon_number')
-        # plt.plot(flist, spn, label=rf'$N={N}$')
-        # plt.ylabel(r'$\langle a^\dag a \rangle/N$')
-        # plt.xlabel(r'$\tilde{F}/\gamma$')
-        # plt.legend()
+        plt.figure(num='scaled_photon_number')
+        plt.plot(flist, spn, label=rf'$N={N}$')
+        plt.ylabel(r'$\langle a^\dag a \rangle/N$')
+        plt.xlabel(r'$\tilde{F}/\gamma$')
+        plt.legend()
 
         plt.figure(num='gap')
         plt.plot(flist, gap/gamma, label=rf'$N={N}$')
